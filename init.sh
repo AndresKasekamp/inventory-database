bf4cc93bd0f80c3d6529ae0c1513f16b3b9af36a
Andres Kasekamp <andreskasekamp@gmail.com>
2024-06-28 16:04:05 +0300

    fixing dockerfile

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ set -e
 export DB_USER
 export DB_PASS
 
+# Ensure required environment variables are set
+if [[ -z "$POSTGRES_USER" || -z "$POSTGRES_DB" || -z "$DB_USER" || -z "$DB_PASS" ]]; then
+    echo "Error: Required environment variables are not set."
+    exit 1
+fi
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE movies;
-    \c movies
+    CREATE DATABASE IF NOT EXISTS movies;
     CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';
     GRANT ALL PRIVILEGES ON DATABASE movies TO $DB_USER;
     GRANT ALL PRIVILEGES ON SCHEMA public TO $DB_USER;

d4cf323c4be0a1d0c828e571638f6ea832f600e5
Andres Kasekamp <andreskasekamp@gmail.com>
2024-06-29 10:44:11 +0300

    fixing dockerfile

--- init.sh
+++ init.sh
@@ -6,20 +6,6 @@ export DB_PASS
 
 /etc/init.d/postgresql start
 
-# psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-#     CREATE DATABASE movies;
-#     CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';
-#     GRANT ALL PRIVILEGES ON DATABASE movies TO $DB_USER;
-#     GRANT ALL PRIVILEGES ON SCHEMA public TO $DB_USER;
-#     CREATE TABLE movies (
-#         id SERIAL PRIMARY KEY,
-#         title VARCHAR(100) NOT NULL,
-#         description VARCHAR(255)
-#     );
-#     GRANT ALL PRIVILEGES ON TABLE movies TO $DB_USER;
-#     GRANT ALL ON SEQUENCE movies_id_seq TO $DB_USER;
-# EOSQL
-
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE DATABASE movies;
     CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';
